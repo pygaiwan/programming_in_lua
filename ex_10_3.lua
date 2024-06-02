1df824f93a6bcf71ffe79b2f527e1694fd88e2d0
pygaiwan <moise.medici@gmail.com>
2024-06-02 17:50:22 +0100

    Add local to test variables

--- ex_10_3.lua
+++ ex_10_3.lua
@@ -15,41 +15,41 @@ end
 
 local original = 'la bella lava il fosso.'
 describe ("Replace each char. All found.\n", function ()
-    s = transliterate(original, {['a'] = 'b', ['l'] = 'd', ['e'] = 'd'})
+    local s = transliterate(original, {['a'] = 'b', ['l'] = 'd', ['e'] = 'd'})
     it('Check length is the same as before', function () assert.is.equal(#s, #original) end)
     it('Check all are replaced', function () assert.is.equal(s, 'db bdddb dbvb id fosso.') end)
 end)
 
 describe ("Replace some char. One not present.\n", function ()
-    s = transliterate(original, {['a'] = 'b', ['l'] = 'd', ['x'] = 'd'})
+    local s = transliterate(original, {['a'] = 'b', ['l'] = 'd', ['x'] = 'd'})
     it('Check length is the same as before', function () assert.is.equal(#s, #original) end)
     it('Check x not replaced', function () assert.is.equal(s, 'db beddb dbvb id fosso.') end)
 end)
 
 describe ("Replace no char. None found.\n", function ()
-    s = transliterate(original, {['x'] = 'b', ['y'] = 'd', ['z'] = 'd'})
+    local s = transliterate(original, {['x'] = 'b', ['y'] = 'd', ['z'] = 'd'})
     it('Check length is the same as before', function () assert.is.equal(#s, #original) end)
     it('Check none replaced', function () assert.is.equal(s, original) end)
 end)
 
 
 describe ("Replace some char. One is false.\n", function ()
-    s = transliterate(original, {['a'] = 'b', ['l'] = false, ['e'] = 'd'})
+    local s = transliterate(original, {['a'] = 'b', ['l'] = false, ['e'] = 'd'})
     it('Check all l removed', function () assert.is.equal(s, 'b bdb bvb i fosso.') end)
 end)
 
 describe ("Remove all chars. All are false.\n", function ()
-    s = transliterate(original, {['a'] = false, ['l'] = false, ['e'] = false})
+    local s = transliterate(original, {['a'] = false, ['l'] = false, ['e'] = false})
     it('Check all mapped char removed', function () assert.is.equal(s, ' b v i fosso.') end)
 end)
 
 describe ("Remove all chars. Result in empty string.\n", function ()
-    s = transliterate('aaaa', {['a'] = false, ['l'] = false, ['e'] = false})
+    local s = transliterate('aaaa', {['a'] = false, ['l'] = false, ['e'] = false})
     it('Check all chars removed', function () assert.is.equal(s, '') end)
 end)
 
 describe ("Original string not modified\n", function ()
-    s = transliterate(original, {['a'] = 'b', ['l'] = 'd', ['e'] = 'd'})
+    local s = transliterate(original, {['a'] = 'b', ['l'] = 'd', ['e'] = 'd'})
     it('Check original is same as before', function () assert.is.equal(original, 'la bella lava il fosso.') end)
 end)
 
