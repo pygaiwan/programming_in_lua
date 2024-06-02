778ca77c00718030f50c84c12a7e09f2a1718ef1
pygaiwan <moise.medici@gmail.com>
2024-06-02 16:49:38 +0100

    Add ex_10_1.lua with busted

---
+++ ex_10_1.lua
@@ -0,0 +1,44 @@
+--- replicate python str.split()
+---@param s string
+---@param delimiter string
+---@return table
+function split (s, delimiter)
+    local count, i, j = 0, 0
+    local t = {}
+    while i do
+        i, j  = s:find(string.format('[^%s]+', delimiter), i)
+        if i then 
+            count = count + 1
+            t[count] = s:sub(i, j)
+            i = j + 1
+        end
+    end
+    return t
+end
+
+describe ("Test space separator on whole string.\n", function ()
+    local t = split("a whole new world", " ")
+    it ("Test correct lenght", function () assert.is.equal(#t, 4) end)
+    it ("Test correct word 1", function() assert.is.equal(t[1], 'a') end)
+    it ("Test correct word 2", function() assert.is.equal(t[2], 'whole') end)
+    it ("Test correct word 3", function() assert.is.equal(t[3], 'new') end)
+    it ("Test correct word 4", function() assert.is.equal(t[4], 'world') end)
+end)
+
+describe ("Test exclamation point in middle of string.\n", function ()
+    local t = split("a whole !new world", "!")
+    it ("Test correct lenght", function () assert.is.equal(#t, 2) end)
+    it ("Test correct slice 1", function() assert.is.equal(t[1], 'a whole ') end)
+    it ("Test correct slice 2", function() assert.is.equal(t[2], 'new world') end)
+end)
+
+describe ("Test missing separator on whole string.\n", function ()
+    local t = split("a whole new world", "s")
+    it ("Test correct lenght", function () assert.is.equal(#t, 1) end)
+    it ("Test whole string returned", function() assert.is.equal(t[1], 'a whole new world') end)
+end)
+
+describe ("Test separator on empty string.\n", function ()
+    local t = split("", "s")
+    it ("Test correct lenght", function () assert.is.equal(#t, 0) end)
+end)
