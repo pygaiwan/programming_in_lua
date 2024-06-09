ef9ceff9fc3ab95c0bffb21b662d04ea5a0dae25
pygaiwan <moise.medici@gmail.com>
2024-06-09 17:22:38 +0100

    Add ex_14_2.lua

---
+++ ex_14_2.lua
@@ -0,0 +1,55 @@
+function list_new() return { first = 0, last = 0 } end
+
+function push_first(list, value)
+    local first = list.first - 1
+    list.first = first
+    list[first] = value
+end
+
+function push_last(list, value)
+    local last = list.last + 1
+    list.last = last
+    list[last] = value
+end
+
+function pop_first(list)
+    local first = list.first
+    if first >= list.last then error('list is empty') end
+    local value = list[first]
+    list[first] = nil
+    list.first = first + 1
+    return value
+end
+
+function pop_last(list)
+    local last = list.last
+    if list.first > last then error('list is empty') end
+    local value = list[last]
+    list[last] = nil
+    list.last = last - 1
+    return value
+end
+
+local l = list_new()
+push_first(l, 5)
+push_first(l, 6)
+push_first(l, 7)
+push_first(l, 8)
+
+push_last(l, "a")
+push_last(l, "b")
+push_last(l, "c")
+
+
+pop_last(l)
+pop_last(l)
+pop_last(l)
+
+pop_first(l)
+pop_first(l)
+pop_first(l)
+pop_first(l)
+
+push_first(l, 8)
+
+for i, c in pairs(l) do print(i, c) end
