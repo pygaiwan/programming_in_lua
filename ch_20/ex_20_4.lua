876e7485ed2b4aa4bda134c305d27a873ec79c31
pygaiwan <moise.medici@gmail.com>
2024-06-23 17:28:15 +0100

    Add ex_20_5 in ex_20_4.lua

--- ch_20/ex_20_4.lua
+++ ch_20/ex_20_4.lua
@@ -7,6 +7,8 @@ local function file_as_array(f_name)
     local content = f:read("a")
     f:close()
 
+    local length = #content
+
     for i = 1, #content do
         proxy[i] = content:sub(i, i)
     end
@@ -21,7 +23,18 @@ local function file_as_array(f_name)
         __newindex = function(_, i, v)
             if i > #proxy then return nil end
             proxy[i] = v
-        end
+        end,
+
+        __len = function() return length end,
+
+        __pairs = function()
+            local k = 0
+            return function()
+                k = k + 1
+                if k <= #proxy then return proxy[k], k end
+            end
+        end,
+
     }
 
     setmetatable(proxy, mt)
@@ -33,3 +46,6 @@ local f = file_as_array('example.txt')
 print(f[1])
 f[1] = 'x'
 print(f[1])
+
+
+for x in pairs(f) do print(x) end
